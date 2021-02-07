c289e51a05dd1858566f5703cbfdd328f9815c7d
Manish Kumar <manish.hit2800@gmail.com>
2021-02-07 15:47:07 +0530

    initial commit

---
+++ counterReactApp/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <header>
+        <h1>Counter App Using State/hooks</h1>
+        <h1>
+          Current Value of Count is:{" "}
+          <span style={{ color: "white" }}>{count}</span>
+        </h1>
+      </header>
+      <div className="App">
+        <button onClick={() => setCount(0)}>Reset Button</button>
+        <button onClick={() => setCount(count + 1)}>Increase Button</button>
+        <button onClick={() => setCount(count - 1)}>Decrease Button</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
